91a50b2e94f61c333e6b716ce4eda45e1db4c92c
lasting <lastingcoder@qq.com>
2023-09-16 23:12:52 +0800

    fix: code can be executed at any moment now

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const targetButton = document.querySelector(".primary-btn.roll-btn");
-const interval = 4000; //时间间隔单位毫秒
-
-let isWindowFocus = true;
+let targetButton = null;
+const interval = 6000; //时间间隔单位毫秒
 let timer = null;
+
+window.addEventListener("load", () => {
+  targetButton = document.querySelector(".primary-btn.roll-btn");
+});
 window.addEventListener("focus", onWindowFocus);
 window.addEventListener("blur", onWindowBlur);
 
 function onWindowFocus() {
+  if (!targetButton) return;
   if (timer) {
     clearInterval(timer);
     timer = null;
